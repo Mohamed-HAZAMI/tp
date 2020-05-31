f0d3a2f5e35a65a7906db82521217dfeb57f537b
mohamedhaz <48091311+mohamedhaz@users.noreply.github.com>
2020-05-31 15:39:59 +0100

    Updates

---
+++ precache-manifest.ca8bcc70cf41ecc1cd7ecd9618a21b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec73d0f5f665bdf2823893c419d387e1",
+    "url": "/tp/index.html"
+  },
+  {
+    "revision": "ca9669842040facb0f5d",
+    "url": "/tp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "76e5c988c89f9d542ea9",
+    "url": "/tp/static/css/main.d6a082cd.chunk.css"
+  },
+  {
+    "revision": "ca9669842040facb0f5d",
+    "url": "/tp/static/js/2.37b62a65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tp/static/js/2.37b62a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e5c988c89f9d542ea9",
+    "url": "/tp/static/js/main.9e9755c2.chunk.js"
+  },
+  {
+    "revision": "5e5e85489e6efc005366",
+    "url": "/tp/static/js/runtime-main.95ef3feb.js"
+  }
+]);
